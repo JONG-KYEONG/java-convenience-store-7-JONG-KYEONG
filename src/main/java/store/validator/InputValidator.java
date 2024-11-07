41ef28c6092ba6caa7729b64036ac7f511bf4065
JongKyeong Kim <oo7bangjk@naver.com>
2024-11-07 17:01:39 +0900

    feat: 프로모션 적용이 가능한 상품에 대해 고객이 해당 수량보다 적게 가져온 경우, 그 수량만큼 추가 여부를 입력받는다.

--- src/main/java/store/validator/InputValidator.java
+++ src/main/java/store/validator/InputValidator.java
@@ -7,29 +7,49 @@ import store.domain.Product;
 import store.view.InputView;
 
 public class InputValidator {
-     private final InputView inputView;
-     InputValidator(InputView inputView){
-         this.inputView = new InputView();
-     }
+    private final InputView inputView;
 
-     public List<Product> getBuyProducts(){
-         while (true) {
-             try {
-                 return makeProductsFromString(inputView.readBuyProduct());
-             } catch (Exception e) {
-                 throw new IllegalArgumentException("[ERROR] 입력 형식을 다시 확인해 주세요");
-             }
-         }
+    InputValidator(InputView inputView) {
+        this.inputView = new InputView();
     }
 
-    private List<Product> makeProductsFromString(String productValue){
-         return Arrays.stream(productValue.split(", "))
-                 .map(productString -> {
-                     String[] parts = productString.replace("[", "").replace("]", "").split("-");
-                     String name = parts[0];
-                     int quantity = Integer.parseInt(parts[1]);
-                     return new Product(name, 0, quantity, null);
-                 })
-                 .collect(Collectors.toList());
+    public List<Product> getBuyProducts(String input) {
+        while (true) {
+            try {
+                return makeProductsFromString(inputView.readBuyProduct());
+            } catch (IllegalArgumentException e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    public boolean plusAdditionalQuantity(String productName, int additionalQuantity) {
+        while (true) {
+            try {
+                String input = inputView.readAdditionalQuantity(productName, additionalQuantity);
+                if (input.equals("Y")) {
+                    return true;
+                }
+                if (input.equals("N")) {
+                    return false;
+                }
+                throw new IllegalArgumentException("[ERROR] Y/N 로만 입력해 주세요.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+    private List<Product> makeProductsFromString(String productValue) {
+        try {
+            return Arrays.stream(productValue.split(", "))
+                    .map(productString -> {
+                        String[] parts = productString.replace("[", "").replace("]", "").split("-");
+                        return new Product(parts[0], 0, Integer.parseInt(parts[1]), null);
+                    })
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new IllegalArgumentException("[ERROR] 입력 형식을 다시 확인해 주세요.");
+        }
     }
 }
