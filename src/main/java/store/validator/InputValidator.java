5b4c7ee7ce04c3e7108faef2781e4074eecfee4c
JongKyeong Kim <oo7bangjk@naver.com>
2024-11-07 17:09:06 +0900

    feat: 멤버십 할인 적용 여부를 입력 받는다.

--- src/main/java/store/validator/InputValidator.java
+++ src/main/java/store/validator/InputValidator.java
@@ -57,6 +57,23 @@ public class InputValidator {
         }
     }
 
+    public boolean isMembershipDiscountApplied() {
+        while (true) {
+            try {
+                String input = inputView.readMembershipDiscount();
+                if (input.equals("Y")) {
+                    return true;
+                }
+                if (input.equals("N")) {
+                    return false;
+                }
+                throw new IllegalArgumentException("[ERROR] Y/N 로만 입력해 주세요.");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e);
+            }
+        }
+    }
+
     private List<Product> makeProductsFromString(String productValue) {
         try {
             return Arrays.stream(productValue.split(", "))
