781e8d9f6ca87b332d7cb95e29171137008fab74
JongKyeong Kim <oo7bangjk@naver.com>
2024-11-07 23:34:01 +0900

    refact: 사용하지 않는 변수 삭제

--- src/main/java/store/constants/Constants.java
+++ src/main/java/store/constants/Constants.java
@@ -3,8 +3,4 @@ package store.constants;
 public class Constants {
     public static final String PRODUCT_RESOURCE_PATH = "src/main/resources/products.md";
     public static final String PROMOTION_RESOURCE_PATH = "src/main/resources/promotions.md";
-    public static final String PROMOTION_TWO_PLUS_ONE = "2+1";
-    public static final String PROMOTION_ONE_PLUS_ONE = "1+1";
-    public static final String PROMOTION_MD_RECOMMENDED = "MD추천상품";
-    public static final String PROMOTION_FLASH_SALE = "반짝할인";
 }
