79ffef20f67bbafeb4d7e7b5ad5bfa15010d8750
JongKyeong Kim <oo7bangjk@naver.com>
2024-11-08 16:49:33 +0900

    fix: 파일 입력 에러 수정

--- src/main/java/store/application/FileReader.java
+++ src/main/java/store/application/FileReader.java
@@ -11,17 +11,22 @@ import store.domain.Promotion;
 
 public class FileReader {
 
-    public List<Product> getProducts(){
+    public List<Product> getProducts() {
         List<String> inputProducts = readProductsFile();
         List<Product> products = new ArrayList<>();
-        for(String product : inputProducts){
-            String [] productValue = product.split(",");
-            products.add(new Product(productValue[0], Integer.parseInt(productValue[1]), Integer.parseInt(productValue[2]), productValue[3]));
+        for (String product : inputProducts) {
+            String[] productValue = product.split(",");
+            if (productValue[0].equals("name")) {
+                continue;
+            }
+            products.add(
+                    new Product(productValue[0], Integer.parseInt(productValue[1]), Integer.parseInt(productValue[2]),
+                            productValue[3]));
         }
         return products;
     }
 
-    public List<String> readProductsFile(){
+    public List<String> readProductsFile() {
         List<String> products = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new java.io.FileReader(Constants.PRODUCT_RESOURCE_PATH))) {
             String line;
@@ -34,18 +39,22 @@ public class FileReader {
         }
     }
 
-    public List<Promotion> getPromotions(){
+    public List<Promotion> getPromotions() {
         List<String> inputPromotions = readPromotionsFile();
         List<Promotion> promotions = new ArrayList<>();
-        for(String promotion : inputPromotions){
-            String [] promotionValue = promotion.split(",");
-            promotions.add(new Promotion(promotionValue[0], Integer.parseInt(promotionValue[1]), Integer.parseInt(promotionValue[2]),
+        for (String promotion : inputPromotions) {
+            String[] promotionValue = promotion.split(",");
+            if (promotionValue[0].equals("name")) {
+                continue;
+            }
+            promotions.add(new Promotion(promotionValue[0], Integer.parseInt(promotionValue[1]),
+                    Integer.parseInt(promotionValue[2]),
                     LocalDate.parse(promotionValue[3]), LocalDate.parse(promotionValue[4])));
         }
         return promotions;
     }
 
-    public List<String> readPromotionsFile(){
+    public List<String> readPromotionsFile() {
         List<String> promotions = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new java.io.FileReader(Constants.PROMOTION_RESOURCE_PATH))) {
             String line;
