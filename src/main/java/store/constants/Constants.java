7e8129313772287d4f8cf777c8d25ce041141a27
JongKyeong Kim <oo7bangjk@naver.com>
2024-11-08 15:34:34 +0900

    feat: 영수증에 총 구매액, 행사 할인, 멤버십 할인을 표시한다.

--- src/main/java/store/constants/Constants.java
+++ src/main/java/store/constants/Constants.java
@@ -3,4 +3,6 @@ package store.constants;
 public class Constants {
     public static final String PRODUCT_RESOURCE_PATH = "src/main/resources/products.md";
     public static final String PROMOTION_RESOURCE_PATH = "src/main/resources/promotions.md";
+    public static final int  MAX_MEMBERSHIP_DISCOUNT_LIMIT = 8000;
+    public static final double MEMBERSHIP_DISCOUNT_RATE = 0.3;
 }
