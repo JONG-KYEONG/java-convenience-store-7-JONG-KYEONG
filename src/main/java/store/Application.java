289dba8bc4c7f4f5a7de982121baed7ac3429b71
JongKyeong Kim <oo7bangjk@naver.com>
2024-11-10 02:17:01 +0900

    refact: TODO 삭제

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -4,7 +4,6 @@ import store.controller.StoreController;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         StoreController storeController = new StoreController();
         storeController.run();
     }
